3ad5425f3a213f7071a8321d314ce6aef184f0fb
Md. Emran Hossain <emran6452@gmail.com>
2025-06-01 18:17:26 +0600

    Added function course_list

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('courses/', views.course_list, name='courses'),
     path('course/<int:course_id>/', views.course_detail, name='course_detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -5,6 +5,10 @@ def home(request):
     courses = Course.objects.all()
     return render(request, 'main/home.html', {'courses': courses})
 
+def course_list(request):
+    courses = Course.objects.all()
+    return render(request, 'main/course_list.html', {'courses': courses})
+
 def course_detail(request, course_id):
     course = get_object_or_404(Course, id=course_id)
     lessons = Lesson.objects.filter(course=course).order_by('order')
